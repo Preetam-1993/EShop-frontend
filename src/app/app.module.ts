23b0bec5300f040be1b12e841351f0da08825ce9
Preetam Mukherjee <preetammukherjee.18@gmail.com>
2021-01-19 20:29:50 +0530

    Code for EShop Front-end Part

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { SigninModalComponent } from './shared/signin-modal/signin-modal.component';
+import { SigninComponent } from './shared/signin/signin.component';
+import { SignupComponent } from './shared/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { HttpClientModule } from '@angular/common/http';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
+    SigninModalComponent,
+    SigninComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
+    TabsModule.forRoot(),
+    HttpClientModule,
+    BsDatepickerModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
